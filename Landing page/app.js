efa2cdacda93c2d34164c90dfec7166fb8f760b5
Danilo paixão <104032841+Danilo-jesus@users.noreply.github.com>
2022-07-21 12:04:55 -0300

    Add files via upload

---
+++ Landing page/app.js
@@ -0,0 +1,9 @@
+function abrirmenu(){
+    let BarraMenuAberto = document.getElementById ('barra-menu');
+    
+    if (BarraMenuAberto.style.width == "0px"){
+        BarraMenuAberto.style.width = "300px";
+    }else{
+        BarraMenuAberto.style.width = "0px";
+    }
+}
